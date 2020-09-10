79f1dc225a70a5664e9bdaa9e2af13516bd1bd5c
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-10 15:09:48 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,27 +1,32 @@
 'use strict';
-const ref = {
-  btnPhoneList: document.querySelector('.js-phone-list-btn'),
 
-  listPhone: document.querySelector('.js-list-phone'),
-  document: document.querySelector('body'),
-};
+phoneShow('.header__box');
+phoneShow('.footer__contacts-box');
 
-ref.btnPhoneList.addEventListener('click', btnPhoneListHandler);
-ref.document.addEventListener('click', bodyHandler);
+function phoneShow(reference) {
+  const ref = {
+    btnPhoneList: document.querySelector(`${reference} .js-phone-list-btn`),
+    listPhone: document.querySelector(`${reference} .js-list-phone`),
+    document: document.querySelector('body'),
+  };
 
-function btnPhoneListHandler() {
-  ref.listPhone.classList.toggle('visually-hidden');
-  ref.btnPhoneList.firstElementChild.classList.toggle('btn__svg--rotate');
-}
+  ref.btnPhoneList.addEventListener('click', btnPhoneListHandler);
+  ref.document.addEventListener('click', bodyHandler);
 
-function bodyHandler(event) {
-  if (
-    event.target === ref.btnPhoneList ||
-    event.target === ref.btnPhoneList.firstElementChild ||
-    event.target === ref.btnPhoneList.firstElementChild.firstElementChild
-  ) {
-    return;
+  function btnPhoneListHandler() {
+    ref.listPhone.classList.toggle('visually-hidden');
+    ref.btnPhoneList.firstElementChild.classList.toggle('btn__svg--rotate');
   }
-  ref.btnPhoneList.firstElementChild.classList.remove('btn__svg--rotate');
-  ref.listPhone.classList.add('visually-hidden');
+
+  function bodyHandler(event) {
+    if (
+      event.target === ref.btnPhoneList ||
+      event.target === ref.btnPhoneList.firstElementChild ||
+      event.target === ref.btnPhoneList.firstElementChild.firstElementChild
+    ) {
+      return;
+    }
+    ref.btnPhoneList.firstElementChild.classList.remove('btn__svg--rotate');
+    ref.listPhone.classList.add('visually-hidden');
+  }
 }
